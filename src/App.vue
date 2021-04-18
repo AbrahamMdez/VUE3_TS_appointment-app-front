0708e69465d46532fedde534f149f61063049887
Abraham <nencho_1@hotmail.com>
2021-04-18 11:27:50 +0200

    copied all files to the old proyect

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>
-    <router-link to="/">Home</router-link>
+  <div id="nav">
   </div>
   <router-view/>
 </template>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap');
 
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+    list-style: none;
+  }
+
+  :root {
+    --primary-color: #C1545A;
+    --secondary-color: #0EEBA8;
+    --terciary-color: #7C7C7C;
+    --cuarternary-color: #FDFDFD;
+    --quintinary-color: #F4F4F4;
+    --color-icons: #0EEBA8;
+    --input-color: #E3E3E3;
+    --font-family: 'Source Sans Pro', sans-serif;
+  }
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-  </div>
+  <body>
+
+    <header>
+      Header
+    </header>
+
+    <main>
+      Main
+    </main>
+  
+    <aside>
+      <router-link to="/">
+        <i class="fab fa-elementor"></i>
+      </router-link>
+    </aside>
+
+  </body>
 </template>
 
 <script lang="ts">
@@ -10,3 +25,44 @@ export default defineComponent({
   name: 'Main'
 });
 </script>
+
+<style scoped>
+
+  body {
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    grid-template-areas: "h h"
+                         "a m"
+                         "a m";
+    grid-template-columns: 20vw 1fr;
+    grid-template-rows: 15vh 1fr;
+  }
+
+  header {
+    grid-area: h;
+    height: 15vh;
+    background-color: var(--cuarternary-color);
+  }
+
+  main {
+    background-color: var(--quintinary-color);
+    grid-area: m;
+    height: 85vh;
+    width: 80vw;
+  }
+
+  aside {
+    grid-area: a;
+    background-color: var(--cuarternary-color);
+    height: 85vh;
+    width: 20vw;
+  }
+
+  aside i {
+    padding: 6vh;
+    font-size: 2rem;
+    color: var(--color-icons);
+  }
+  
+</style>
