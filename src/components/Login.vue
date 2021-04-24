a40e196e578a8b5616d2afeaa6aa72f1ca80447d
Abraham <nencho_1@hotmail.com>
2021-04-24 11:37:33 +0200

    created login component and form to login user but it does not have logic still

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,24 @@
 <template>
-  
+    <form @submit.prevent="">
+        <label>Email</label>
+        <input type="email" placeholder="Email">
+
+        <label>Contraseña</label>
+        <input type="text" placeholder="Contraseña">
+
+        <input type="submit" value="Acceder">
+    </form>
 </template>
 
 <script>
 export default {
+    name: 'Login',
+
+    data() {
+        return {
+            
+        }
+    },
 
 }
 </script>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -2,24 +2,33 @@
   <form @submit.prevent="saveClient">
       <label>Nombre</label>
       <input type="text" v-model="newClient.name" placeholder="Nombre">
+
       <label>Apellidos</label>
       <input type="text" v-model="newClient.surname" placeholder="Apellidos">
+
       <label>Teléfono</label>
       <input type="number" v-model="newClient.phoneNumber" placeholder="Teléfono"> 
+
       <label>Correo Electrónico</label>
       <input type="email" v-model="newClient.email" placeholder="Correo electrónico">
+
       <label>NIF</label>
       <input type="text" v-model="newClient.nif" placeholder="NIF">
+
       <!-- <label>Contraseña</label>
       <input type="text" v-model="newClient.pass1" placeholder="Contraseña">
+
       <label>Repetir Contraseña</label>
       <input type="text" v-model="newClient.pass2" placeholder="Repetir contraseña"> -->
+      
       <input type="submit" value="Crear">
   </form>
 </template>
 
 <script>
 export default {
+    name: 'Register',
+
     data() {
         return {
             newClient: {
@@ -33,16 +42,18 @@ export default {
             }
         }
     },
+    
     methods: {
         saveClient() {
             this.axios.post('clients/create-client', this.newClient)
-                .then(res =>{
-                 this.newClient = res.data
-                 console.log(res.data)
-                 })
-                .catch(err => console.error(err))
+                .then( res => {
+                    this.newClient = res.data
+                    console.log(res.data)
+                })
+
+                .catch( err => console.error(err));
         }
-    },
+    }
 }
 </script>
 
